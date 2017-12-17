2c71c8c371375fb2d8b5fb92111663571fd91e0d
jz4o <26317360+jz4o@users.noreply.github.com>
2017-12-17 18:30:16 +0900

    チームクラスを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Team
+Team.delete_all
+('A'..'C').each do |alpha|
+  Team.create(name: "#{alpha}チーム")
+end
