1bb645c3b199a40ce29945b5fa626f488e39759e
jz4o <26317360+jz4o@users.noreply.github.com>
2017-12-19 21:40:42 +0900

    Railsアプリケーションの別ブランチ移動に伴い、関連ファイルを削除

--- app/views/members/_member.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! member, :id, :code, :name, :gender, :age, :team_id, :created_at, :updated_at
-json.url member_url(member, format: :json)
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'teams#index'
-
-  resources :teams
-  resources :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
